d3854abb06c6155c043ec7cdff14ac82fd81b886
Refael Yzg <refael.yzgeav@solaredge.com>
2021-12-22 15:36:30 +0200

    Change banner componenent - change background image

--- src/components/Banner0.jsx
+++ src/components/Banner0.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Button } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import QueueAnim from 'rc-queue-anim';
 import TweenOne from 'rc-tween-one';
 import { isImg } from './utils';
+
 
 class Banner extends React.PureComponent {
   
@@ -32,9 +32,6 @@ class Banner extends React.PureComponent {
           <div key="content" {...dataSource.content}>
             {dataSource.content.children}
           </div>
-          <Button ghost key="button" {...dataSource.button}>
-            {dataSource.button.children}
-          </Button>
         </QueueAnim>
         <TweenOne
           animation={{
